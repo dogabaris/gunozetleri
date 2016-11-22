84b2630dd743b701249381e679fdafa3c15a06b0
dogabaris <dogabarisozdemir@gmail.com>
2016-11-22 19:59:14 +0200

    facebook page publish eklendi

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var request = require('request');
 var bodyParser = require('body-parser');
+//facebook app token
 var token = "EAAZAudLVqZCMEBAHBo7poRvZBLwTOmAWkj5XCN3rhVk1Lgg2FJJFfJNrs59StOwQTjoBQt6YGcrCML524hWikZBPSo0raJE5LyXKeRpqvO1tsN56ldZBdD2NkDgbZBhZBMxY2aGrGTOJjeL7ZBE8zZA2gHO9KHbp26VRr5CPovgxExQZDZD"
 var Twitter = require('twitter');
 
@@ -12,17 +13,18 @@ var client = new Twitter({
   access_token_secret: 'EkQtQDv4mwoHX1jtEZsiJeZA6evlnAGNOcE1vYZydLDrr'
 });
 
+//örnek fotoğraf
 var data = require('fs').readFileSync('gazete-mansetleri.jpg');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended : true}));
 
-// Test
+// Web sayfası çalışırlık testi
 app.get('/', function(req , res){
 	res.send('world!')
 });
 
-// Verify
+// Facebook application doğrulama
 app.get('/webhook', function(req, res) {
 	if (req.query['hub.verify_token'] === 'verify-token') {
 			res.send(req.query['hub.challenge']);
@@ -32,32 +34,7 @@ app.get('/webhook', function(req, res) {
 	}
 });
 
-app.get('/twitter', function(req, res) {
-	client.post('media/upload', {media: data}, function(error, media, response) {
-
-  if (!error) {
-
-    // If successful, a media object will be returned.
-    console.log(media);
-
-    // Lets tweet it
-    var status = {
-      status: 'Resimli gün özeti',
-      media_ids: media.media_id_string // Pass the media id string
-    }
-
-    client.post('statuses/update', status, function(error, tweet, response) {
-      if (!error) {
-        console.log(tweet);
-      }
-    });
-
-  }
-	})
-	res.sendStatus(200);
-});
-
-
+// Fb Messenger handle
 app.post("/webhook/", function(req, res) {
 	messaging_events = req.body.entry[0].messaging;
 	for (i = 0; i < messaging_events.length; i++) {
@@ -80,6 +57,58 @@ app.post("/webhook/", function(req, res) {
 	}
 	res.sendStatus(200);
 });
+
+//Twitter
+app.get('/twitter', function(req, res) {
+	client.post('media/upload', {media: data}, function(error, media, response) {
+
+  if (!error) {
+    // başarılı olursa istek obje dönecek
+    //console.log(media);
+    // tweet atılacak
+    var status = {
+      status: 'Resimli gün özeti',
+      media_ids: media.media_id_string // Media id stringi atanacak
+    }
+
+    client.post('statuses/update', status, function(error, tweet, response) {
+      if (!error) {
+        console.log(tweet);
+      }
+    });
+
+  }
+	})
+	res.sendStatus(200);
+});
+
+app.get('/facebook', function(req, res) {
+  sendPagePost();
+	res.sendStatus(200);
+});
+
+//Fonksiyonlar
+
+//pageid/feed e istek yollandığında sayfaya server üzerinden gün özeti atılır.
+function sendPagePost(sender, text) {
+	messageData = {
+		text: text
+	};
+	request({
+		url: "https://graph.facebook.com/v2.8/1676849415962995/feed",
+		qs: { access_token: token },
+		method: "POST",
+		json: {
+			message: "Gün Özeti"
+		}
+	}, function(error, response, body) {
+		if (error) {
+			console.log("Error sending message: ", error);
+		} else if (response.body.error) {
+			console.log("Error: ", response.body.error);
+		}
+	});
+}
 
 function sendTextMessage(sender, text) {
 	messageData = {
